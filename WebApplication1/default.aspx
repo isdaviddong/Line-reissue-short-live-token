46a59c76b7c24aa59572cf5727642ac5dc982c08
David Tung on MSDN <isdaviddong@hotmail.com>
2018-08-24 17:11:54 +0800

    v1

---
+++ WebApplication1/default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="WebApplication1._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="font-weight: 700">
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Re-Issue Token" />
+&nbsp;<br />
+            <br />
+            token: <asp:TextBox ID="TextBox1" runat="server" Rows="5" TextMode="MultiLine" Width="300px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Send Sticker With New Token" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using isRock.LineBot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        const string Channel_ID = "!!!請換成你的Channel_ID!!!"; //例如 -->  160XXXXX37
+        const string Channel_Secret = "!!!請換成你的Channel_Secret!!!";  //例如 -->  9d6e49bxxxxxxxxxx90xxxx863bd69e7
+        const string AdminUserId = "!!!請換成你的AdminUserId!!!"; //例如 -->  U6bXXXXXXXXX3422XXXX341bXX6b8XXXX
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //重新issue一個 short-lived token
+            var token = isRock.LineBot.Utility.IssueChannelAccessToken(Channel_ID, Channel_Secret);
+            //顯示在TextBox1中
+            this.TextBox1.Text = token.access_token;
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            //用TextBox1中的新token發送訊息
+            var token = this.TextBox1.Text;
+            var bot = new Bot(token);
+            //發貼圖給admin
+            bot.PushMessage(AdminUserId, 1, 2);
+        }
+    }
+}
